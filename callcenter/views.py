5627ae1e50d5d0993f37b74086cd245270d29513
sudo-Whateverman <nick@smartgreen.co.il>
2017-02-11 00:44:41 +0200

    -- added profiles and videoconferences model -- added about / index pages to app -- added app called callcenter -- changed requirements.txt

--- callcenter/views.py
+++ callcenter/views.py
@@ -5,10 +5,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
-@login_required
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
 
 # Add this view
 class AboutPageView(TemplateView):
