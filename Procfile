378f0c2fb08066396df1dcebccd4326bd061ae2a
sudo-Whateverman <nick@smartgreen.co.il>
2017-02-10 22:06:18 +0200

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vc4life.wsgi
---
+++ vc4life/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for vc4life project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vc4life.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
