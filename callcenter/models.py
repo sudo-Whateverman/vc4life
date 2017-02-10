b193161b323d574865a8b34df21d922d4e7fa289
sudo-Whateverman <nick@smartgreen.co.il>
2017-02-11 00:38:22 +0200

    -- added profiles and videoconferences model -- added about / index pages to app -- added app called callcenter -- changed requirements.txt

--- callcenter/models.py
+++ callcenter/models.py
@@ -1,3 +1,32 @@
-from django.db import models
-
 # Create your models here.
+import uuid
+from django.db import models
+from django.utils import timezone
+
+
+class Profile(models.Model):
+    profile = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    Level = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    pikud = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class VideoCall(models.Model):
+    status_field = ['pending approval', 'approved', 'completed']
+    status = models.TextField(choices=status_field, default='pending approval')
+    VC_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    request_time = models.DateTimeField(
+        default=timezone.now()
+    )
+    starting_time = models.DateTimeField(
+        default=timezone.now())
+    participants = models.TextField()  # change this to many to one key
+    # add more conversation properties
+
+    def __str__(self):
+        return self.VC_id
--- callcenter/views.py
+++ callcenter/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
+@login_required
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
