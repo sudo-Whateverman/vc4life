0f7328ef6e1df9b8300e1a5c61edbe6b55a823a8
sudo-Whateverman <nick@smartgreen.co.il>
2017-02-13 20:48:22 +0200

    -- extended user addition to include automatic profile

--- callcenter/admin.py
+++ callcenter/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 import callcenter
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
+
 admin.site.register(callcenter.models.VideoCall)
 admin.site.register(callcenter.models.Profile)
 admin.site.register(callcenter.models.VCallcenter)
+
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
